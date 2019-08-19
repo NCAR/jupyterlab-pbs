e7257d132922ff3fac346ee3e620ba9932ca3730
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-08-18 20:42:25 -0600

    Apply pre-commit hooks

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,48 @@
 import {
-  JupyterFrontEnd, JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from "@jupyterlab/application";
 
-import {
-  ICommandPalette, MainAreaWidget
-} from '@jupyterlab/apputils';
+import { ICommandPalette, MainAreaWidget } from "@jupyterlab/apputils";
 
-import {
-  Widget
-} from '@phosphor/widgets';
+import { Widget } from "@phosphor/widgets";
 
-import '../style/index.css';
-
+import "../style/index.css";
 
 /**
  * Initialization data for the jupyterlab-pbs extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-pbs',
+  id: "jupyterlab-pbs",
   autoStart: true,
   requires: [ICommandPalette],
   activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyterlab-pbs is activated!');
-
+    console.log("JupyterLab extension jupyterlab-pbs is activated!");
 
     // create a blank content widget inside of a MainAreaWidget
     const content = new Widget();
-    const widget = new MainAreaWidget({content});
-    widget.id = 'jupyterlab-pbs';
-    widget.title.label = 'PBS Pro Workload Manager';
+    const widget = new MainAreaWidget({ content });
+    widget.id = "jupyterlab-pbs";
+    widget.title.label = "PBS Pro Workload Manager";
     widget.title.closable = true;
 
-    // Add an application command 
-    const command: string = 'pbs:open';
+    // Add an application command
+    const command: string = "pbs:open";
     app.commands.addCommand(command, {
-      label: 'PBS Queue',
+      label: "PBS Queue",
       execute: () => {
-        if (!widget.isAttached){
+        if (!widget.isAttached) {
           // Attach the widget to the main work area if it is not there
-          app.shell.add(widget, 'main');
+          app.shell.add(widget, "main");
         }
 
-        // Activate the widget 
+        // Activate the widget
         app.shell.activateById(widget.id);
       }
     });
 
     // Add the command to the palette.
-    palette.addItem({command, category: 'HPC Tools'})
+    palette.addItem({ command, category: "HPC Tools" });
   }
 };
 
