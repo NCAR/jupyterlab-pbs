e765c9a67085e2036a63f0d2b13d984baf2cadb0
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-06-15 14:07:40 -0600

    Cookiecutter template overhaul

---
+++ setup.py
@@ -0,0 +1,82 @@
+"""
+Setup Module to setup Python Handlers for the jupyterlab-pbs extension.
+"""
+import os
+
+import setuptools
+from jupyter_packaging import (
+    combine_commands,
+    create_cmdclass,
+    ensure_python,
+    ensure_targets,
+    get_version,
+    install_npm,
+)
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The name of the project
+name = 'jupyterlab_pbs'
+
+# Ensure a valid python version
+ensure_python('>=3.5')
+
+# Get our version
+version = get_version(os.path.join(name, '_version.py'))
+
+lab_path = os.path.join(HERE, name, 'labextension')
+
+# Representative files that should exist after a successful build
+jstargets = [
+    os.path.join(HERE, 'lib', 'jupyterlab-pbs.js'),
+]
+
+package_data_spec = {name: ['*']}
+
+data_files_spec = [
+    ('share/jupyter/lab/extensions', lab_path, '*.tgz'),
+    ('etc/jupyter/jupyter_notebook_config.d', 'jupyter-config', 'jupyterlab_pbs.json'),
+]
+
+cmdclass = create_cmdclass(
+    'jsdeps', package_data_spec=package_data_spec, data_files_spec=data_files_spec
+)
+
+cmdclass['jsdeps'] = combine_commands(
+    install_npm(HERE, build_cmd='build:all', npm=['jlpm']), ensure_targets(jstargets),
+)
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name=name,
+    version=version,
+    url='https://github.com/NCAR/jupyterlab-pbs',
+    author='Anderson Banihirwe',
+    description=' A JupyterLab extension to interface with PBS scheduler',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    cmdclass=cmdclass,
+    packages=setuptools.find_packages(),
+    install_requires=['jupyterlab~=2.0',],
+    zip_safe=False,
+    include_package_data=True,
+    license='BSD-3-Clause',
+    platforms='Linux, Mac OS X, Windows',
+    keywords=['Jupyter', 'JupyterLab',],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Jupyter',
+    ],
+)
+
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_args)
